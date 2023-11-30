49c1fa9d3b1dd0f6792197ab7328f3650fd94796
Aymen <yahyaouiaymen@rocketmail.com>
2023-11-30 16:07:55 +0100

    Handling Null and Undefined

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,10 +11,21 @@ export class ProductDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
+    this.product =   {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2021",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
   }
 
 }
