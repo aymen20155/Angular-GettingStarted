ddec05976b811ef4529889db269cba68e982e7aa
Aymen <yahyaouiaymen@rocketmail.com>
2023-12-02 00:09:33 +0100

    Add sharedModule

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,27 +1,23 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { convertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { convertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { SharedModule } from '../shared/shared.module';
+import { ProductDetailComponent } from './product-detail.component';
 import { ProductDetailGuard } from './product-detail.guard';
+import { ProductListComponent } from './product-list.component';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     convertToSpacesPipe,
-    StarComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {path: 'products/:id', canActivate:[ProductDetailGuard], component: ProductDetailComponent},
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
